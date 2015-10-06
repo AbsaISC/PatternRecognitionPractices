e85c99beeae85ea23f04329976b7390ae746ec04
Absa <absalomaraujo@gmail.com>
2015-10-05 23:39:50 -0500

    practica successful finished

--- plotClasses.m
+++ plotClasses.m
@@ -7,20 +7,22 @@ function plotClasses( classes , vector, means)
 	if size(vector,2)==2
         % msgbox('grafiga 2d')
         vleg=[];
+        symbol=[];
+        color=[];
 		for classs=1:size(classes,3)
-			vcol=randColorAndType();
+			[vcol symbol color]=randColorAndType(symbol , color);
 			plot(classes(1,:,classs), classes(2,:,classs), vcol ,'MarkerSize',10,'LineWidth',3)
 			if classs==1
 				vleg= ['clase #',num2str(classs)];
 			else
 				vleg=cat(1, vleg, ['clase #',num2str(classs)]);
 			end
-			plot(means(1,classs), means(2,classs), vcol, 'MarkerSize', 15, 'LineWidth',3)
-			vleg=cat(1, vleg, ['media #',num2str(classs)]);
-			x=[means(1,classs),vector(1)];
-			y=[means(2,classs),vector(2)];
-			plot(x,y)
-			vleg=cat(1, vleg, ['linea #',num2str(classs)]);
+			%plot(means(1,classs), means(2,classs), vcol, 'MarkerSize', 15, 'LineWidth',3)
+			%vleg=cat(1, vleg, ['media #',num2str(classs)]);
+			%x=[means(1,classs),vector(1)];
+			%y=[means(2,classs),vector(2)];
+			%plot(x,y)
+			%vleg=cat(1, vleg, ['linea #',num2str(classs)]);
 		end
 		plot(vector(1), vector(2), 'rh', 'MarkerSize', 20, 'LineWidth',5)
 		legend(vleg)
@@ -29,21 +31,24 @@ function plotClasses( classes , vector, means)
 	if size(vector,2)==3
         % msgbox('grafica 3d')
         vleg=[];
+
+        symbol=[];
+        color=[];
 		for classs=1:size(classes,3)
-			vcol= randColorAndType();
+			[vcol symbol color]=randColorAndType(symbol , color);
 			plot3(classes(1,:,classs), classes(2,:,classs),classes(3,:,classs),vcol,'MarkerSize',10,'LineWidth',3)
 			if classs==1
 				vleg= ['clase #',num2str(classs)];
 			else
 				vleg=cat(1, vleg, ['clase #',num2str(classs)]);
 			end
-			plot3(means(1,classs), means(2,classs),means(3,classs), vcol, 'MarkerSize', 15, 'LineWidth',3)
-			vleg=cat(1, vleg, ['media #',num2str(classs)]);
-			x=[means(1,classs),vector(1)];
-			y=[means(2,classs),vector(2)];
-			z=[means(3,classs),vector(3)];
-			plot3(x,y,z)
-			vleg=cat(1, vleg, ['linea #',num2str(classs)]);
+			% plot3(means(1,classs), means(2,classs),means(3,classs), vcol, 'MarkerSize', 15, 'LineWidth',3)
+			% vleg=cat(1, vleg, ['media #',num2str(classs)]);
+			% x=[means(1,classs),vector(1)];
+			% y=[means(2,classs),vector(2)];
+			% z=[means(3,classs),vector(3)];
+			% plot3(x,y,z)
+			% vleg=cat(1, vleg, ['linea #',num2str(classs)]);
 		end
 		plot3(vector(1), vector(2),vector(3), 'rh', 'MarkerSize', 20, 'LineWidth',5)
 		legend(vleg)
--- randColorAndType.m
+++ randColorAndType.m
@@ -1,4 +1,4 @@
-function resFinal = randColorAndType( )
+function [resFinal colors symbols] = randColorAndType(colors, symbols)
            % b     blue          .     point              -     solid
            % g     green         o     circle             :     dotted
            % r     red           x     x-mark             -.    dashdot 
@@ -13,65 +13,23 @@ function resFinal = randColorAndType( )
            %                     p     pentagram
            %                     h     hexagram
 %rng(0,'twister');
-color = randi([1 7],1,1);
-switch color
-  case 1
-    res='b';
-  case 2
-    res='g';
-  case 3
-    res='r'; 
-  case 4
-    res='c';
-  case 5
-    res='m';
-  case 6
-    res='y'; 
-  case 7
-    res='k';
-  % case 8
-  %   res='w'; 
+if(isempty(colors))
+  colors=[ 'b', 'g', 'r','c','m','y','k'];
 end
+tamc=size(colors,2);
+color = randi([1 tamc],1,1);
 
-symbol = randi([1 11],1,1);
-switch symbol
-  case 1
-    res2='.';
-  case 2
-    res2='o';
-  case 3
-    res2='x'; 
-  case 4
-    res2='+';
-  case 5
-    res2='*';
-  case 6
-    res2='s'; 
-  case 7
-    res2='d';
-  case 8
-    res2='v'; 
-  case 9
-    res2='^';
-  case 10
-    res2='<';
-  case 11
-    res2='>'; 
-  % case 12
-  %   res2='p';
-  % case 13
-  %   res2='h';
-  % case 14
-  %   res2='-'; 
-  % case 15
-  %   res2=':';
-  % case 16
-  %   res2='-.'; 
-  % case 17
-  %   res2='--'; 
+if(isempty(symbols))
+  symbols=['.', 'o', 'x', '+', '*', 's', 'd', 'v','^', '<', '>'];
 end
+tams=size(symbols,2);
+symbol = randi([1 tams],1,1);
 
-resFinal=strcat(res,res2);
+unox=colors(color);
+dosx=symbols(symbol);
+colors(color)=[];
+symbols(symbol)=[];
+resFinal=strcat(unox,dosx);
 return ;
 end
 
